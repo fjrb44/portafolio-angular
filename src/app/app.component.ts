ded029cc4e2ec4039916303f860cadd559632dd2
Francisco Javier <franciscojavier.rios.bautista@iesvegademijas.es>
2018-12-28 15:03:51 +0100

    Fin de la sección 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor( public _infoPagina: InfoPaginaService ) {
 
+export class AppComponent {
+  constructor( 
+    public _infoPagina: InfoPaginaService, 
+    public productosService: ProductosService
+  ) {
   }
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,13 +10,26 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+  }
+
+  private cargarInfo(){
     this.http.get("assets/data/data-pagina.json")
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-
-        console.log(resp);
       });
+  }
+
+  private cargarEquipo(){
+    this.http.get("https://angular-html-25cf9.firebaseio.com/equipo.json")
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+    });
   }
 }
